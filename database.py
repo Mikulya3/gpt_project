1d85c6ac3ed825401055213a5ffb07c0bd15410a
hello <kadirbekova43@gmail.com>
2024-06-06 23:22:17 +0300

    database.py

--- database.py
+++ database.py
@@ -1,7 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
+import yaml
 
-engine = create_engine('postgresql://hello:1@localhost/gpt_4', echo=True)
+def load_config():
+    with open('config.yaml', 'r') as file:
+        config = yaml.safe_load(file)
+        return config
+
+config = load_config()
+database_url = config['database_url']
+
+
+engine = create_engine(database_url, echo=True)
 Base = declarative_base()
 Session = sessionmaker()
